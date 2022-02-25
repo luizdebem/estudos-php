522166040d6b91a6cf457b00d3c9f5d7068de2ce
luizdebem <luizdebem99@gmail.com>
2022-02-25 17:24:01 -0300

    test gitignore

--- controllers/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-  global $pdo;
-  $pdo = new PDO("mysql:host=localhost;port=3306;db=store", '', '');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo->query("USE store;");
